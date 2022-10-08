ae71904212326a1556a030712071e8bbac58d83c
quantumde <89156762+quantumde@users.noreply.github.com>
2022-10-08 15:18:17 +0300

    Delete build/all/app directory

--- build/all/app/install/src/post_wall.py
+++
@@ -1,4 +0,0 @@
-import vk_api
-
-def post_wall_message(text):
-  print(vk.wall.post(message=text))
--- build/all/app/po/CMakeFiles/uvk.quantumdev.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/uvk.quantumdev.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/uvk.quantumdev.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
