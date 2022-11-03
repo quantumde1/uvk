e3a82de303f12cef1276ebcebbbcf9baabf74dfa
meserakovarsenij810@gmail.com <stalinizm121@gmail.com>
2022-11-03 21:56:12 +0300

    ecfdvcsgv f

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     qDebug() << "Starting app from main.cpp";
 
     QQuickView *view = new QQuickView();
-    view->setSource(QUrl("qrc:/harbour-cat.qml"));
+    view->setSource(QUrl("qrc:/Main.qml"));
     view->setResizeMode(QQuickView::SizeRootObjectToView);
     view->show();
 
